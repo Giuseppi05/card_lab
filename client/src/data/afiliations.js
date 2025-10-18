1c5a51d8be0dead1cb817033e421507f6ec6eddd
Giuseppi Saavedra <giuseppi0501@gmail.com>
2025-10-17 19:53:21 -0500

    add affiliation icons

--- client/src/data/afiliations.js
+++ client/src/data/afiliations.js
@@ -1,36 +1,32 @@
 // Configuración de afiliaciones
 const AFILIATIONS = [
   { label: "Ninguna", value: "" },
-  { label: "Piratas Mugiwara", value: "afiliaciones/piratas_mugiwara.png" },
-  { label: "Piratas Buggy", value: "afiliaciones/piratas_buggy.png" },
   { label: "Alianza Saruyama", value: "afiliaciones/alianza_saruyama.png" },
-  { label: "Franky Family", value: "afiliaciones/franky_family.png" },
-  {
-    label: "Nuevos Piratas Gyojin",
-    value: "afiliaciones/nuevos_piratas_gyojin.png",
-  },
-  { label: "Piarats Bonnie", value: "afiliaciones/piarats_bonnie.png" },
+  { label: "Baroque Works", value: "afiliaciones/baroque_works.png" },
+  { label: "Familia Franky", value: "afiliaciones/franky_family.png" },
+  { label: "Nuevos Piratas Gyojin", value: "afiliaciones/nuevos_piratas_gyojin.png" },
   { label: "Piratas Apoo", value: "afiliaciones/piratas_apoo.png" },
   { label: "Piratas Arlong", value: "afiliaciones/piratas_arlong.png" },
+  { label: "Piratas Barbablanca", value: "afiliaciones/piratas_barbablanca.png" },
   { label: "Piratas Bege", value: "afiliaciones/piratas_bege.png" },
   { label: "Piratas Bellamy", value: "afiliaciones/piratas_bellamy.png" },
-  {
-    label: "Piratas Blackbeard",
-    value: "afiliaciones/piratas_blackbeard.png",
-  },
-  {
-    label: "Piratas Doflamingo",
-    value: "afiliaciones/piratas_Doflamingo.png",
-  },
+  { label: "Piratas Barbanegra", value: "afiliaciones/piratas_blackbeard.png" },
+  { label: "Piratas Bonnie", value: "afiliaciones/piarats_bonnie.png" },
+  { label: "Piratas Buggy", value: "afiliaciones/piratas_buggy.png" },
+  { label: "Piratas Doflamingo", value: "afiliaciones/piratas_Doflamingo.png" },
   { label: "Piratas Foxy", value: "afiliaciones/piratas_foxy.png" },
   { label: "Piratas Hawkins", value: "afiliaciones/piratas_hawkins.png" },
+  { label: "Piratas Kuja", value: "afiliaciones/piratas_kuja.png" },
   { label: "Piratas Law", value: "afiliaciones/piratas_law.png" },
   { label: "Piratas Moria", value: "afiliaciones/piratas_moria.png" },
+  { label: "Piratas Mugiwara", value: "afiliaciones/piratas_mugiwara.png" },
+  { label: "Piratas Pelirrojo", value: "afiliaciones/piratas_pelirrojo.png" },
   { label: "Piratas Roshio", value: "afiliaciones/piratas_roshio.png" },
   { label: "Piratas Sol", value: "afiliaciones/piratas_sol.png" },
   { label: "Piratas Uroge", value: "afiliaciones/piratas_uroge.png" },
   { label: "Piratas World", value: "afiliaciones/piratas_world.png" },
   { label: "Piratas X-Drake", value: "afiliaciones/piratas_X-Drake.png" },
 ];
+
 
 export default AFILIATIONS;
